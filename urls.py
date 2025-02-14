0b9f05b44bbf6b4df1a72fb48089242a25ec0dbb
Onorio de Jesus Afonso <onorio@catalpa.io>
2025-02-14 11:04:23 +0900

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('create-account', views.create_account, name='create_account'),
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
 ]
